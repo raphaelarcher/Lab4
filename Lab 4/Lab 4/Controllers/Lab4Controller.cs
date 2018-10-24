4703aec2e6f01c3bb6c0597df592ce0597857632
Raphael Archer <archerr3@go.stockton.edu>
2018-10-24 17:11:12 -0400

    trying to get right project to publish

---
+++ Lab 4/Lab 4/Controllers/Lab4Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaffleGenerator;
+
+namespace Lab_4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["TimeOfDay"] = timeOfDay();
+            DateTime date = DateTime.Now;
+            return View(date);
+        }
+
+        public string timeOfDay()
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                return "Good Morning";
+            }
+            else if (DateTime.Now.Hour < 18)
+            {
+                return "Good Afternoon";
+            }
+            else
+            {
+                return "Good Evening";
+            }
+        }
+
+        public IActionResult Waffler()
+        {
+            ViewData["TimeOfDay"] = timeOfDay();
+            return View("Waffler");
+        }
+    }
+}
